ff329d48f11c1b46207663bec9f0c52ac699c0db
Kai French <kai.french2004@gmail.com>
2021-10-25 20:56:30 +0100

    I successfully Enumed
    Yaaaaaaaay!

--- Program.cs
+++ Program.cs
@@ -4,41 +4,69 @@ namespace Project_for_Dad
 {
     class Program
     {
+        enum skills
+        {
+            Strength=0,
+            Dexterity,
+            Sorcery,
+            Pyromancy,
+            Miracles,
+            Challenge,
+            Cosplay,
+        }
+
+        enum challenges
+        {
+            Broken_Straight_Sword=0,
+            Pryo_Only,
+            Sorcery_Only,
+            Miracle_Only,
+            Soul_Level_One,
+        }
+
+        enum cosplays
+        {
+            Gundyr=0,
+            Watcher,
+            Dancer,
+            Dragonslayer,
+            Ornstein,
+            Lorian,
+            Cinder,
+            Nameless,
+            Friede,
+            Gael,
+            Artorias
+        }
         static void Main(string[] args)
         {
-            string skillsanswer;
-            string challengesanswer;
-            string cosplaysanswer;
-            string[] skills = {"Strength", "Dexterity", "Sorcery", "Pyromancy", "Miracles", "Challenge Run", "Cosplay Run"};
-            string[] challenges =
-                {"Broken Straight Sword", "Pyro Only", "Sorcery Only", "Miracles Only", "Soul Level 1"};
-            string[] cosplays =
-            {
-                "Iudex Gundyr", "Abyss Watcher", "Dancer", "Dragonslayer Armour/Ornstein", "Lorian", "Soul of Cinder",
-                "Nameless King", "Sister Friede", "Slave Knight Gael", "Artorias", "Smough"
-            };
 
-            Random rand = new Random();
-            int index = rand.Next(skills.Length);
-            skillsanswer = Convert.ToString(skills[index]);
-            Random rando = new Random();
-            int indextwo = rando.Next(challenges.Length);
-            challengesanswer = Convert.ToString(challenges[indextwo]);
-            Random randow = new Random();
-            int indexthree = randow.Next(challenges.Length);
-            cosplaysanswer = Convert.ToString(cosplays[indexthree]);
+            var rand = new Random();
+            var skillCount = Enum.GetNames(typeof(skills)).Length;
+            var skillInt = rand.Next(skillCount);
+            var skillStr = (skills) skillInt;
 
-            if (skillsanswer == "Challenge Run")
+            var rando = new Random();
+            var challengeCount = Enum.GetNames(typeof(challenges)).Length;
+            var challengeInt = rando.Next(challengeCount);
+            var challengeStr = (challenges) challengeInt;
+
+            var randow = new Random();
+            var cosplayCount = Enum.GetNames(typeof(cosplays)).Length;
+            var cosplayInt = randow.Next(cosplayCount);
+            var cosplayStr = (cosplays) cosplayInt;
+            
+            if (skillInt == 5)
             {
-                Console.WriteLine("You are doing a {0} challenge run", challengesanswer);
+                Console.WriteLine("You are doing a {0} challenge run", challengeStr);
             }
-            else if (skillsanswer == "Cosplay Run")
+            else if (skillInt == 6)
             {
-                Console.WriteLine("You are doing a {0} cosplay run", cosplaysanswer);
+                Console.WriteLine("You are doing a {0} cosplay run", cosplayStr);
             }
             else
             {
-                Console.WriteLine("You are doing a standard run using {0} as your primary skill", skillsanswer);
+                Console.WriteLine("You are doing a standard run using {0} as your primary skill", skillStr);
             }
         }
     }
